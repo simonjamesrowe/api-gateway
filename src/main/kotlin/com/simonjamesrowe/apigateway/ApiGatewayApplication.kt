76e27b98c4314ffbc87b95fcd308c6a1c34bcc9b
Simon Rowe <simon.rowe@gmail.com>
2021-03-06 18:13:09 +0000

    attempt to fix tracing for webflux apps.

--- src/main/kotlin/com/simonjamesrowe/apigateway/ApiGatewayApplication.kt
+++ src/main/kotlin/com/simonjamesrowe/apigateway/ApiGatewayApplication.kt
@@ -1,13 +1,20 @@
 package com.simonjamesrowe.apigateway
 
 import co.elastic.apm.attach.ElasticApmAttacher
+import co.elastic.apm.opentracing.ElasticApmTracer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 @EnableFeignClients
-class ApiGatewayApplication
+class ApiGatewayApplication {
+
+  @Bean
+  fun apmTracer() = ElasticApmTracer()
+
+}
 
 fun main(args: Array<String>) {
   ElasticApmAttacher.attach()
