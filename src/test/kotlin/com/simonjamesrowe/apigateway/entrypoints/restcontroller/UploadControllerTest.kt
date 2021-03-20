ae4f535ff106b896444789d7c139499482a1fff9
Simon Rowe <simon.rowe@gmail.com>
2021-03-20 09:07:57 +0000

    refactor for clean code.

--- src/test/kotlin/com/simonjamesrowe/apigateway/entrypoints/restcontroller/UploadControllerTest.kt
+++ src/test/kotlin/com/simonjamesrowe/apigateway/entrypoints/restcontroller/UploadControllerTest.kt
@@ -11,10 +11,10 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
 import org.springframework.context.annotation.Import
-import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
 
@@ -22,7 +22,7 @@ import org.springframework.test.web.reactive.server.WebTestClient
 @ActiveProfiles("cms")
 @Import(SecurityConfig::class)
 @AutoConfigureWireMock(port = 0)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+@AutoConfigureWebTestClient(timeout = "20000")
 internal class UploadControllerTest {
 
   @MockkBean
